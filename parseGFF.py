bac3b606031952e813d132a88c54876332dc47fe
astrose <astrose@uark.edu>
2020-04-06 13:53:11 -0500

    parsing FASTA files

--- parseGFF.py
+++ parseGFF.py
@@ -1,54 +1,32 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import os"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#open file\n",
-    "input_file=open(watermelon.gff\", \"r\")\n",
-    "\n",
-    "#for each line in watermelon.gff do the following\n",
-    "for line in input_file:\n",
-    "    #split fields by space\n",
-    "    fields = [line.split() for line in input_file]\n",
-    "    #sort the fields by col 9 in ascending order\n",
-    "    fields = (list.sort(fields, key = lambda x: x[8]))\n",
-    "    #skip if files are similar after scanning, do not run the code\n",
-    "    for line in fields:\n",
-    "        if (line[8] == \"similar\"):\n",
-    "            continue\n",
-    "        else:\n",
-    "            #otherwise print gene names in a list in alphabetical order\n",
-    "            gene_name = line[8]\n",
-    "        print(genes_names)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import argparse
+import csv
+from Bio import Se2qIO
+
+#create variable parser
+parser=argparse.ArgumentParser(description=This script parses GFF files, and will do more later")
+
+#add positional arguments
+parser.add_argument("gff", help="name of the GFF file")
+parser.add_argument("fasta", help="name of the FASTA file")
+
+#parsethe arguments
+args = parser.parse_args()
+
+#read and parse the FASTA file
+genome=SeqIO.read(args.fasta, "fasta")
+
+#read GFFfile, line by line
+with open(args.gff, "r") as gff_file:
+	reader = csv.reader(gff_file, delimiter="\t")
+	for line in reader:
+		#skip blank lines
+		if not line:
+			continue
+		else:
+			start= line [3]
+			end  = line [4]
+			pint(start, end)
+			
+
+#list gene names
+gene_names = []
